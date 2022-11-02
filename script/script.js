b98261b59799d763f37a14e05ca865a160fede65
James <james@precious1.net>
2022-11-02 20:51:45 +0100

    Images, finishing touches, extra translations

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-const projName = document.getElementById("projName");
+ projName = document.getElementById("projName");
 const projText = document.getElementById("projText");
 const projLang = document.getElementById("projLang");
 const projTool = document.getElementById("projTool");
@@ -8,6 +8,7 @@ const abtText = document.getElementById("abtText");
 const proficienciesTitle = document.getElementById("proficienciesTitle");
 const proficienciesText = document.getElementById("proficienciesText");
 const hireText = document.getElementById("whyHire");
+const carousel = document.getElementById("projCarousel");
 
 const projectBtns = document.getElementsByClassName("projectBtn");
 const cardTitles = document.getElementsByClassName("card-caption");
@@ -15,11 +16,12 @@ const cardTexts = document.getElementsByClassName("card-text");
 
 let projects = JSON.parse(projectsEN);
 let generalText = JSON.parse(generalEN);
-let currentLang = "";
+let projectImages = JSON.parse(projImg);
+let currentLang = "btnradio1";
 let currProject = 0;
 
 const cardBtns = document.querySelectorAll('[data-button="card"]');
-console.log(cardBtns);
+console.log(projectImages);
 
 mainText();
 changeCards();
@@ -49,6 +51,7 @@ function changeLang(language) {
     changeCardBtns(lang);
     changeText(currProject);
     changeCards();
+    changeExtras();
     mainText();
 }
 
@@ -65,10 +68,23 @@ function mainText() {
             projectBtns[i].innerText = projects[i].projectName;
         }
         catch {
-            projectBtns[i].innerHTML = "<i>No project</i>";
+            if (currentLang == "btnradio1") {
+                projectBtns[i].innerHTML = "<i>COMING SOON</i>";
+            }
+            else {
+                projectBtns[i].innerHTML = "<i>KOMT BINNENKORT</i>";
+            }
             projectBtns[i].className += " disabled"
         }
     }
+
+    if (currentLang == "btnradio1") {
+        document.querySelector("#othersText").innerText = "Others"
+    }
+    else {
+        document.querySelector("#othersText").innerText = "Overig"
+    }
+
 }
 
 function changeCardBtns(lang) {
@@ -105,11 +121,66 @@ function changeText(projectNr) {
         projText.innerHTML = "Sorry, this project cannot be found!"
         console.log(error);
     }
+
+    updateCarousel(projectNr);
 }
 
 function changeCards() {
     for (let i = 0; i < cardTitles.length; i++) {
         cardTitles[i].innerText = projects[i].projectName;
         cardTexts[i].innerText = projects[i].cardText;
+    }
+}
+
+function updateCarousel(projectNr) {
+    carousel.innerHTML = "";
+
+    for (let i = 0; i < projectImages[projectNr].img[1]; i++) {
+        let carItem = document.createElement("div");
+        let carImg = document.createElement("img");
+
+        carImg.className = "d-block w-100";
+        carImg.src = "asset/image/projects/" + projectImages[projectNr].img[0] + "/" + i + ".png";
+        carItem.append(carImg);
+
+        carItem.className = "carousel-item";
+
+        if (i == 0) {
+            carItem.className += " active";
+        }
+        carousel.append(carItem);
+        console.log("test");
+    }
+
+    if (projectImages[projectNr].img[1] <= 1) {
+        document.querySelector(".carousel-control-prev").hidden = true;
+        document.querySelector(".carousel-control-next").hidden = true;
+    }
+    else {
+        document.querySelector(".carousel-control-prev").hidden = false;
+        document.querySelector(".carousel-control-next").hidden = false;
+    }
+}
+
+function changeExtras() {
+    if (currentLang == "btnradio1") {   
+        document.querySelector("#abtLink").innerHTML = "About&nbsp;Me";
+        document.querySelector("#profLink").innerHTML = "Proficiencies";
+        document.querySelector("#projLink").innerHTML = "Projects";
+        document.querySelector("#contLink").innerHTML = "Contact";
+        document.querySelector("#emailText").innerHTML = "&nbsp;Email&nbsp;Me!";
+        document.querySelector("#langText").innerHTML = "Languages";
+        document.querySelector("#welcomeText").innerHTML = "Welcome to my portfolio.";
+        document.querySelector("#scrollBounce").innerHTML = "&darr; Scroll Down &darr;";
+    }
+    else {
+        document.querySelector("#abtLink").innerHTML = "Over&nbsp;Mij";
+        document.querySelector("#profLink").innerHTML = "Vaardigheden";
+        document.querySelector("#projLink").innerHTML = "Projecten";
+        document.querySelector("#contLink").innerHTML = "Contact";
+        document.querySelector("#emailText").innerHTML = "&nbsp;Mail&nbsp;Mij!";
+        document.querySelector("#langText").innerHTML = "Talen";
+        document.querySelector("#welcomeText").innerHTML = "Welkom op mijn portfolio.";
+        document.querySelector("#scrollBounce").innerHTML = "&darr; Scroll Omlaag &darr;";
     }
 }
